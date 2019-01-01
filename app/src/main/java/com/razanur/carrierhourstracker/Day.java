a5df82f29b671b69e5be32765b975256b82f3ba6
Casey English <kcpisme@gmail.com>
2019-01-01 13:16:25 -0800

    Added field to store if a date is excluded. Added function to check if date is excluded.
    This required updating the Schema version number to 2, and creating a
    migration path. After migrating, deleted migration path, as there are
    no more v1 schemas in existence.

--- app/src/main/java/com/razanur/carrierhourstracker/Day.java
+++ app/src/main/java/com/razanur/carrierhourstracker/Day.java
@@ -5,6 +5,12 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 
 @Entity(tableName = "day_table")
 public class Day {
@@ -16,6 +22,7 @@ public class Day {
     private double mStartTime;
     private double mEndTime;
     private boolean mNsDay;
+    private boolean mExcluded;
     private double mHoursWorked;
     private double mStraightTime;
     private double mOvertime;
@@ -27,10 +34,12 @@ public class Day {
         mEndTime = endTime;
         mNsDay = nsDay;
 
-        setHoursWorked(calcHoursWorked());
-        setStraightTime(calcStraightTime());
-        setOvertime(calcOvertime());
-        setPenalty(calcPenalty());
+        mExcluded = determineIfExcluded();
+
+        mHoursWorked = calcHoursWorked();
+        mStraightTime = calcStraightTime();
+        mOvertime = calcOvertime();
+        mPenalty = calcPenalty();
     }
 
     private Day(String date) {
@@ -59,7 +68,7 @@ public class Day {
         int prime = 37;
         int result = 1;
 
-        result = prime * result + ((mDate == null) ? 0 : mDate.hashCode());
+        result = prime * result +  mDate.hashCode();
 
         return result;
     }
@@ -83,14 +92,85 @@ public class Day {
     }
 
     private double calcOvertime() {
-        if (!mNsDay)
-            return Math.min(mHoursWorked - mStraightTime, 2.0);
+        if (!mNsDay) {
+            if (!mExcluded)
+                return Math.min(mHoursWorked - mStraightTime, 2.0);
+            else
+                return mHoursWorked-mStraightTime;
+        }
         else
            return Math.min(mHoursWorked, 8.0);
     }
 
     private double calcPenalty() {
-        return Math.max(mHoursWorked -(mStraightTime + mOvertime), 0.0);
+        if (!mExcluded)
+            return Math.max(mHoursWorked -(mStraightTime + mOvertime), 0.0);
+        else
+            return 0;
+    }
+
+    private boolean determineIfExcluded() {
+        String format = "MM/dd/yyyy";
+        String exclusionStart;
+        String exclusionEnd;
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, Calendar.DECEMBER);
+        cal.set(Calendar.YEAR, Integer.valueOf(mDate.substring(6)));
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+
+        switch (cal.get(Calendar.DAY_OF_WEEK)) {
+            case 1:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)-1);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            case 2:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+5);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            case 3:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+4);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            case 4:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+3);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            case 5:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+2);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            case 6:
+                cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+1);
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+            default:
+                exclusionStart = sdf.format(cal.getTime());
+                break;
+        }
+
+        cal.set(Calendar.DAY_OF_YEAR, cal.get(Calendar.DAY_OF_YEAR)+27);
+        exclusionEnd = sdf.format(cal.getTime());
+
+        Date startDate;
+        Date endDate;
+        Date date;
+
+        try {
+            startDate = sdf.parse(exclusionStart);
+            endDate = sdf.parse(exclusionEnd);
+            date = sdf.parse(mDate);
+
+            if ((date.after(startDate) && date.before(endDate)) ||
+                    (mDate.equals(exclusionStart)) || mDate.equals(exclusionEnd)) {
+                return true;
+            }
+        } catch (ParseException e) {
+            // We only get here if the strings are badly formatted, but they are formatted by
+            // sdf.format(), so we'll never get here.
+        }
+        return false;
     }
 
     public String getDate() {
@@ -107,6 +187,14 @@ public class Day {
 
     public double getEndTime() {
         return mEndTime;
+    }
+
+    public boolean isExcluded() {
+        return mExcluded;
+    }
+
+    public void setExcluded(boolean excluded) {
+        mExcluded = excluded;
     }
 
     public double getHoursWorked() {
